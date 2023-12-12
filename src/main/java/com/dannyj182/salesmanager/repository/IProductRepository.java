bed5142b03f9e0bf346fe5772a5e629b03997648
Danny Alexander Jimenez Casares <dannyj182@gmail.com>
2023-12-11 21:18:42 -0300

    Removed an unnecessary method in IProductRepository

--- src/main/java/com/dannyj182/salesmanager/repository/IProductRepository.java
+++ src/main/java/com/dannyj182/salesmanager/repository/IProductRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IProductRepository extends CrudRepository<Product, Long> {
-    Boolean existsByProductId(Long id);
 }
