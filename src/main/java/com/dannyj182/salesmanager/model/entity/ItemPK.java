09874543b81cbf9c5e03fea7d6c93de62d150b7a
Danny Alexander Jimenez Casares <dannyj182@gmail.com>
2023-12-22 23:27:57 -0300

    Override equals and hashCode methods in ItemPK class

--- src/main/java/com/dannyj182/salesmanager/model/entity/ItemPK.java
+++ src/main/java/com/dannyj182/salesmanager/model/entity/ItemPK.java
@@ -5,10 +5,28 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 @Getter @Setter
 public class ItemPK implements Serializable {
+
     private Long saleId;
     private Long productId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ItemPK that = (ItemPK) o;
+
+        return Objects.equals(saleId, that.saleId) &&
+                Objects.equals(productId, that.productId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(saleId, productId);
+    }
 }
